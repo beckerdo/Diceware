f504b5155ac289e0c216aabc29af57f054cfa0fb
beckerdo <dan@danbecker.info>
2015-05-25 15:04:15 -0500

    Implement passphrase

--- src/main/java/info/danbecker/diceware/Diceware.java
+++ src/main/java/info/danbecker/diceware/Diceware.java
@@ -7,6 +7,15 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Random;
+import java.security.SecureRandom;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
 
 /**
  * Diceware
@@ -21,20 +30,127 @@ import java.util.ArrayList;
  * @author <a href="mailto://dan@danbecker.info>Dan Becker</a>
  */
 public class Diceware {
-	public static final String STD_LIST_NAME = "diceware.txt";
-	public static final String EXT_LIST_NAME = "diceware8k.txt";	
+	public static final String DICT_STD_NAME = "diceware.txt";
+	public static final String DICT_D8K_NAME = "diceware8k.txt";	
+	
+	public static final String WORD_DELIM = " ";
+	public static final String SPECIAL_CHARS = "~!#$%^&*()-=+[]\\{}:;\"'<>?/0123456789";
 
 	public static final int MAX_5DICE = 7775;  // aka "66666"	
 	public static final int MAX_8K = 8191;
 	
-	private List<String> wordList = new ArrayList<String>(9000);
+	private static final List<String> wordList = new ArrayList<String>(9000);
+	private static final Random random = new SecureRandom();
 	
 	public static final org.slf4j.Logger jLogger = 
 		org.slf4j.LoggerFactory.getLogger(Diceware.class);
 
+	public enum WordOption {
+		NONE, ONE, ALL
+	}
+	
+	public enum DictionaryOption {
+		STD, D8K, OTHER
+	}
+	
+	// options
+	public static boolean strictMode = false; // uses dice method, not computer shortCuts
+	public static WordOption specialCharEntropy = WordOption.ONE; // use EnumSet.of( ONE, TWO ) for multiples	
+    public static int numWords = 1;
+	public static DictionaryOption dictionary = DictionaryOption.D8K;
+	public static String dictionaryName = DICT_D8K_NAME;
+	
 	public static void main(String[] args) throws Exception {
+		// Parse command line options
+		parseGatherOptions( args );
+		System.out.println( "Diceware numWords=" + numWords );
+		System.out.println( "Diceware specialCharEntropy=" + specialCharEntropy.toString() );
+		System.out.println( "Diceware stricMode=" + strictMode );
+		System.out.println( "Diceware dictionary=" + dictionary );
+		System.out.println( "Diceware dictionary name=" + dictionaryName );
+		
+		// load dictionary
+		loadList( Diceware.wordList, dictionaryName );
+		
+		String passPhrase = getPassPhrase( Diceware.wordList, numWords, specialCharEntropy );
+		System.out.println( "Diceware passPhrase=" + passPhrase );
 	}
 
+	/** Command line options for this application. */
+	public static void parseGatherOptions( String [] args ) throws ParseException {
+		// Parse the command line arguments
+		Options options = new Options();
+		options.addOption( "h", "help", false, "print the command line options." );
+		options.addOption( "n", "numWords", true, "uses this many words in the pass-phrase." );
+		options.addOption( "e", "specialCharEntropy", true, "uses special characters in NONE, ONE, ALL words." );
+		options.addOption( "s", "strictMode", false, "avoids computer random short cuts, uses diceware dice simulation." );
+		options.addOption( "d", "dictionary", true, "dictionary STD (standard), D8K (8k list), OTHER (name provided)." );
+		options.addOption( "r", "dictionaryName", true, "dictionary name" );
+
+		CommandLineParser cliParser = new DefaultParser();
+		CommandLine line = cliParser.parse( options, args );
+		
+	    // Gather command line arguments for execution
+	    if( line.hasOption( "help" ) ) {
+	    	HelpFormatter formatter = new HelpFormatter();
+	    	formatter.printHelp( "java -jar diceware.jar <options> info.danbecker.diceware.Diceware", options );
+	    	System.exit( 0 );
+	    }
+	    if( line.hasOption( "numWords" ) ) {
+	    	numWords = Integer.parseInt( line.getOptionValue( "numWords" ) );
+	    }	  		
+	    
+	    if( line.hasOption( "strictMode" ) ) {
+	    	strictMode = true;	
+	    }	  		
+	    if( line.hasOption( "specialCharEntropy" ) ) {
+	    	specialCharEntropy = WordOption.valueOf( line.getOptionValue( "specialCharEntropy" ) );
+	    }	  		
+	    if( line.hasOption( "dictionaryName" ) ) {
+	    	dictionaryName = line.getOptionValue( "dictionaryName" );
+	    }	  		
+	    if( line.hasOption( "dictionary" ) ) {
+	    	dictionary = DictionaryOption.valueOf( line.getOptionValue( "dictionary" ) );
+	    	
+	    	switch (dictionary) {
+    			case STD: dictionaryName = DICT_STD_NAME;
+    			case D8K: dictionaryName = DICT_D8K_NAME;
+    			case OTHER: // do nothing, use provided name
+	    	}
+	    }	  		
+	}
+	
+	// TODO Implement strict mode
+	
+	public static String getPassPhrase( final List<String> wordList, int numWords, WordOption specialChars) {
+		StringBuilder sb = new StringBuilder();
+		
+		// Choose a single random word.
+		int randomWord = -1;
+		if ( WordOption.ONE == specialChars ) {
+			randomWord = random.nextInt(numWords);
+		}
+		
+		for ( int i = 0; i < numWords; i++ ) {
+			if ( i > 0 )
+				sb.append( Diceware.WORD_DELIM );
+			
+			String word = wordList.get( random.nextInt( wordList.size() ));
+			
+			// Special characters?
+			if (( WordOption.ALL == specialChars ) || (( WordOption.ONE == specialChars ) && ( i == randomWord ))) {
+				int randomPosition = random.nextInt(  word.length() );
+				char [] chars = word.toCharArray();
+				chars[ randomPosition ] = getRandomSpecialChar();
+				word = new String( chars );
+			}
+			
+			sb.append( word );			
+		}
+		
+		return sb.toString();
+	}
+	
 	/** 
 	 * Converts an int such as 1, 6, 123, 654321 to base 6 String.
 	 * The string is ordinal adjusted to 0 => "1"
@@ -85,4 +201,8 @@ public class Diceware {
 	    }
 	}
 	
+    public static char getRandomSpecialChar() {
+    	int pos = random.nextInt( SPECIAL_CHARS.length() );
+    	return SPECIAL_CHARS.charAt( pos );
+    }
 }
--- src/test/java/info/danbecker/diceware/DicewareTest.java
+++ src/test/java/info/danbecker/diceware/DicewareTest.java
@@ -2,12 +2,16 @@ package info.danbecker.diceware;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.StringTokenizer;
 
+import org.apache.commons.cli.ParseException;
 import org.junit.Test;
 import org.junit.Before;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertNotNull;
 
 public class DicewareTest {
 	@Before
@@ -69,16 +73,94 @@ public class DicewareTest {
 		try {
 			List<String> wordList = new ArrayList<String>(9000);
 			
-			Diceware.loadList( wordList, Diceware.STD_LIST_NAME );			
+			Diceware.loadList( wordList, Diceware.DICT_STD_NAME );			
 			assertEquals( "word list", 7776, wordList.size() );
 			wordList.clear();
 			
-			Diceware.loadList( wordList, Diceware.EXT_LIST_NAME );			
+			Diceware.loadList( wordList, Diceware.DICT_D8K_NAME );			
 			assertEquals( "word list", 8192, wordList.size() );
 			wordList.clear();
 		} catch ( Throwable e ) {
 			System.out.println( "exception=" + e);
 			assertNull( "word list", e);
+		}
+		
+	}
+
+	@Test
+    public void testOptions() {
+		String [] cmdLine = new String[] { "java", "Diceware", "--numWords=5", "--specialCharEntropy=ALL", "--strictMode" };
+
+		try{ 
+			Diceware.parseGatherOptions( cmdLine );
+			assertEquals( "options", 5, Diceware.numWords );
+			assertEquals( "options", Diceware.WordOption.ALL, Diceware.specialCharEntropy );
+			assertEquals( "options", true, Diceware.strictMode );
+		} catch( ParseException e ) {
+			assertNull( "options", e);
+		}
+		
+	}
+	
+	@Test
+    public void testSpecialChars() {
+		assertEquals( "special chars", 36, Diceware.SPECIAL_CHARS.length() );
+		assertEquals( "special chars", '~', Diceware.SPECIAL_CHARS.charAt( 0 ) );
+		assertEquals( "special chars", '9', Diceware.SPECIAL_CHARS.charAt( Diceware.SPECIAL_CHARS.length() - 1 ) );		
+		assertEquals( "special chars", '\\', Diceware.SPECIAL_CHARS.charAt( 15 ));		
+	}
+	
+	@Test
+    public void testGetPassPhrase() {
+		try {
+			List<String> wordList = new ArrayList<String>(9000);
+			Diceware.loadList( wordList, Diceware.DICT_D8K_NAME );			
+			assertEquals( "pass phrase", 8192, wordList.size() );
+			
+			final int NUM_WORDS = 6;
+			String passPhrase = Diceware.getPassPhrase( wordList, NUM_WORDS, Diceware.WordOption.NONE );
+			System.out.println( "Diceware random passPhrase=" + passPhrase );
+			assertNotNull( "pass phrase", passPhrase );
+			
+			StringTokenizer st = new StringTokenizer( passPhrase, Diceware.WORD_DELIM );
+			assertEquals( "pass phrase", NUM_WORDS, st.countTokens() );
+			
+			while( st.hasMoreTokens() ) {
+				String word = st.nextToken();
+				// Hmm, some single chars in there
+				assertTrue( "pass phrase", word.length() > 0  );
+				
+				// Hmm, some words have numbers or special chars like ~!@#$%^&*()_+
+				// for (int i = 0; i < word.length(); i++){
+				//     char c = word.charAt(i);
+				// 	assertTrue( "pass phrase alpha word=" + word + ", position=" + i, Character.isAlphabetic( c )  );
+				// 	assertTrue( "pass phrase lower word=" + word + ", position=" + i, Character.isLowerCase( c )  );
+				// }
+			}
+			
+			passPhrase = Diceware.getPassPhrase( wordList, NUM_WORDS, Diceware.WordOption.ALL );
+			System.out.println( "Diceware random passPhrase=" + passPhrase );
+			assertNotNull( "pass phrase", passPhrase );
+			
+			st = new StringTokenizer( passPhrase, Diceware.WORD_DELIM );
+			assertEquals( "pass phrase", NUM_WORDS, st.countTokens() );
+			
+			while( st.hasMoreTokens() ) {
+				String word = st.nextToken();
+				assertTrue( "pass phrase", word.length() > 0  );
+
+				int specialCount = 0;
+				for (int i = 0; i < word.length(); i++){
+				    char c = word.charAt(i);
+				    if ( -1 != Diceware.SPECIAL_CHARS.indexOf( c ))
+				    	specialCount++;
+				}
+				assertTrue( "pass phrase special count", specialCount > 0 );
+			}
+
+		} catch ( Throwable e ) {
+			System.out.println( "exception=" + e);
+			assertNull( "pass phrase", e);
 		}
 		
 	}
